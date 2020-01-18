ac84c3bbcac75f1ca3ef60e2a97fb911a03099a4
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-01-18 22:53:40 +0200

    Sidebar tests

--- server.js
+++ server.js
@@ -3,42 +3,63 @@ var http = require("http");
 var fs = require("fs");
 
 var HTMLTagNo = "<!-- Server_No_Replace -->";
+var server;
+var defaultPage = "home.html";
 
 function StartServer() {
-	var server = http.createServer((function(request, response)
-		{
-			var fileName = request.url.slice(1, request.url.length);
-			if (fileName.indexOf(".") === -1) {fileName += ".html"};
-			var fileExtension = fileName.slice(fileName.lastIndexOf(".")+1, fileName.length);
-			console.log(fileName, fileExtension);
-			if (fs.existsSync(fileName)) {
-				response.writeHead(200, {"Content-Type": ReturnContentTypeBasedOnFileExtension(fileExtension)});
-				fs.readFile(fileName, 'utf8', function(err, _contents) {
-					var contents;
-					if (fileExtension === "html") {
-						contents = ProcessHTML(_contents);
-					} else {
-						contents = _contents;
-					}
-					response.write(contents);
+	//console.log(server !== undefined);
+	//console.log(server);
+	
+	if (server === undefined) {
+		server = http.createServer((function(request, response)
+			{
+				var fileName = request.url.slice(1, request.url.length);
+				if (fileName === "") {fileName = defaultPage;}
+				if (fileName.indexOf(".") === -1) {fileName += ".html"};
+				var fileExtension = fileName.slice(fileName.lastIndexOf(".")+1, fileName.length);
+				//console.log(fileName, fileExtension);
+				if (fs.existsSync(fileName)) {
+					response.writeHead(200, {"Content-Type": ReturnContentTypeBasedOnFileExtension(fileExtension)});
+					fs.readFile(fileName, 'utf8', function(err, _contents) {
+						var contents;
+						if (fileExtension === "html") {
+							contents = ProcessHTML(_contents);
+						} else {
+							contents = _contents;
+						}
+						response.write(contents);
+						response.end();
+					});	
+				} else {
+					response.writeHead(404, {"Content-Type": "text/plain"});
+					response.write("File " + fileName + " not found!");
 					response.end();
-				});	
-			} else {
-				response.writeHead(404, {"Content-Type": "text/plain"});
-				response.write("File " + fileName + " not found!");
-				response.end();
+				}
 			}
-		}
-	));
-	server.listen(7000);
+		));
+		server.listen(7000);
+		console.log("The server is now listening on port 7000!");
+	};
 }
 var baseHTMLFileName = "BaseHTML.html";
 var baseHTML = "";
 var baseHTMLToReplace = '<wholepage></wholepage>';
-fs.readFile(baseHTMLFileName, 'utf8', function(err, contents) {
-	baseHTML = contents;
-	StartServer();
-});	
+
+function LoadBaseHTML() {
+	fs.readFile(baseHTMLFileName, 'utf8', function(err, contents) {
+		baseHTML = contents;
+		StartServer();
+	});
+};
+
+fs.watch("BaseHTML.html", {}, (eventType, filename) => {
+  if (filename) {
+    //console.log(filename);
+    LoadBaseHTML();
+  }
+});
+
+LoadBaseHTML();
 
 var ContentTypes = {
 	"html":	"text/html",
